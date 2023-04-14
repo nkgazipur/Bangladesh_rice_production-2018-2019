6a61e2b48db8992e65df3fa4019ad6801b500e8c
nkgazipur <97935097+nkgazipur@users.noreply.github.com>
2023-04-14 14:05:01 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -264,7 +264,7 @@ const drawMap = (
     .attr("class", "legend-group")
     .attr(
       "transform",
-      `translate(${(width - margin.left - margin.right) / 2 + 50}, ${
+      `translate(${(width - margin.left - margin.right) / 2 + 10}, ${
         margin.top + 150
       })`
     );
